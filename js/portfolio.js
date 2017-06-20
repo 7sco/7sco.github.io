1b3f5bc8c843ee03276716ee18e0ae29baa0219f
7sco <ISCO@Franciscos-MacBook-Pro.local>
2017-06-20 14:14:12 -0400

    Portfolio-v3.1 jquery fixed2.0

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
- $(".hello").animate({fontSize: '2.5em', width: "70%"}, 1000);
+ $(".hello").animate({fontSize: '2.5em', width: "40%"}, 1000);
  $(".name").fadeIn(2000);
  $(".right").slideDown(2000);
        

4b6fe6fcb2213b60653e24cae13e4e069de8c82a
7sco <xaviandrade14@gmail.com>
2017-06-20 14:18:42 -0400

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
- $(".hello").animate({fontSize: "2em", width: "50%"}, 1000);
+ $(".hello").animate({fontSize: "2em", width: "40%"}, 1000);
  $(".name").fadeIn(2000);
  $(".right").slideDown(2000);
        
